07f79e3d0128625728cda731424127c1afc150b0
Will Huang <willhuang85@gmail.com>
2014-08-24 12:28:07 -0500

    Adding simple error checking and ui input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 jQuery(function($){
 
 	$('#getNext').click(function(){
-		var liveCells = [[2,1],[2,2],[2,3]]; 
+//		var liveCells = [[2,1],[2,2],[2,3]];
+
+        var input = $('#myInput').val();
+        var M = $('#M').val();
+        var N = $('#N').val();
+        if (!input || !M || !N)
+            return;
+        var liveCells = $.parseJSON($('#myInput').val());
 		var data = {
-			'M': 5,
-			'N': 5,
+			'M': M,
+			'N': N,
 			'liveCells' : liveCells
 		};
 
@@ -14,9 +21,16 @@ jQuery(function($){
 			dataType: 'jsonp',
 			data: data,
 			success: function(response) {
-				$('#response').text(response);
-			} 
+				$('#response').text(JSON.stringify(response));
+			},
+            error: function(x) {
+                $('#response').text(JSON.parse(x.responseText).error);
+            }
 		});
 
 	});
+
+
+
+
 });
